5ce01f39fc0f22a044e908d306cf6e8415fe2e08
Szymon Scibik <scibik.szym@gmail.com>
2026-03-22 15:50:43 +0100

    add minssing config got tests

--- tests/TWAction.IntegrationTests/TWActionWebApplicationFactory.cs
+++ tests/TWAction.IntegrationTests/TWActionWebApplicationFactory.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Npgsql;
@@ -48,6 +49,17 @@ public sealed class TWActionWebApplicationFactory : WebApplicationFactory<Progra
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test");
+
+        builder.ConfigureAppConfiguration((_, config) =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["GeneratorApi:BaseUrl"] = "http://localhost:9999",
+                ["GeneratorApi:ApiKey"] = "test-api-key",
+                ["PlemionaRozpiskiApi:BaseUrl"] = "http://localhost:9998",
+                ["PlemionaRozpiskiApi:ApiKey"] = "test-api-key",
+            });
+        });
 
         builder.ConfigureServices(services =>
         {
